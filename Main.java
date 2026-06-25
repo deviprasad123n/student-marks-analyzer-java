1295fd8f1a0bd450c99b11d062703edc73ed4eef
deviprasad123n <ravilladeviprasad35@gmail.com>
2026-06-25 15:33:23 +0530

    Added initial Main.java file

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Student Marks Analyzer ===");
+        System.out.println("Git workflow is working, and Java is ready!");
+    }
+}
